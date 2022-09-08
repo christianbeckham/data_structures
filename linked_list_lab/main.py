bc9fd1f9312cf77eee1a1ae41dfd1af08e31522f
Christian Beckham <urenacb22@gmail.com>
2022-09-08 14:42:29 -0500

    feat: created Node and LinkedList classes and methods for adding and finding nodes in Linked List

---
+++ linked_list_lab/main.py
@@ -0,0 +1,10 @@
+from linked_list import LinkedList
+
+linked_list = LinkedList()
+
+linked_list.append_node(10)
+linked_list.append_node(33)
+linked_list.append_node(97)
+
+linked_list.find_node(570)
+linked_list.find_node(33)
